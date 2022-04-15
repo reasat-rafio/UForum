d39c52ad06174bd8d5bc9a54231f6e74e4521c6c
reasat-rafio <rafioar96@gmail.com>
2022-04-15 14:22:15 +0600

    :hammer: navbar added

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { AnimatePresence } from "framer-motion";
+import { ManagedUIContext } from "@contexts/ui.context";
+import { Navbar } from "@components/navbar";
+
+function handleExitComplete() {
+  if (typeof window !== "undefined") {
+    window.scrollTo({ top: 0 });
+  }
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AnimatePresence exitBeforeEnter onExitComplete={handleExitComplete}>
+      <ManagedUIContext>
+        <Navbar />
+        <Component {...pageProps} />;
+      </ManagedUIContext>
+    </AnimatePresence>
+  );
+}
+
+export default MyApp;
