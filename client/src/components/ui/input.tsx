aede8510ea93671a0cef2163efe21d5f30b3d02e
reasat-rafio <rafioar96@gmail.com>
2022-04-15 16:03:25 +0600

    :hammer: registration page added

---
+++ client/src/components/ui/input.tsx
@@ -0,0 +1,63 @@
+import React, { InputHTMLAttributes } from "react";
+import clsx from "clsx";
+
+export interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  inputClassName?: string;
+  labelKey?: string;
+  placeholderKey?: string;
+  name: string;
+  errorKey?: string;
+  type?: string;
+  shadow?: boolean;
+}
+
+const Input = React.forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      className = "block",
+      labelKey,
+      name,
+      errorKey,
+      placeholderKey,
+      shadow = false,
+      type = "text",
+      inputClassName,
+      ...rest
+    },
+    ref
+  ) => {
+    const rootClassName =
+      "w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm";
+
+    return (
+      <div className={className}>
+        {labelKey && (
+          <label
+            htmlFor={name}
+            className="block text-gray-600 font-semibold text-sm leading-none mb-3 cursor-pointer"
+          >
+            {labelKey}
+          </label>
+        )}
+        <input
+          id={name}
+          name={name}
+          type={type}
+          ref={ref}
+          // @ts-ignore
+          placeholder={placeholderKey}
+          className={rootClassName}
+          autoComplete="off"
+          spellCheck="false"
+          aria-invalid={errorKey ? "true" : "false"}
+          {...rest}
+        />
+        {errorKey && <p className="my-2 text-xs text-red-500">{errorKey}</p>}
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
+export default Input;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,23 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        "2xl": "3rem",
+      },
+    },
     extend: {
       colors: {
         primary: "#1682FD",
         secondary: "#F48023",
         "secondary-hover": "#c26419",
         "light-gray": "#EAEAEA",
+        "light-gray-2": "#FAFAFA",
         "light-gray-hover": "#bababa",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
