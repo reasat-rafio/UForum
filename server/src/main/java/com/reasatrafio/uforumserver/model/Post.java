84c65d4cd60565ddb13e7b76ccc13ef9e1c2c991
reasat-rafio <rafioar96@gmail.com>
2022-04-17 05:20:00 +0600

    :sparkles: post model added

--- server/src/main/java/com/reasatrafio/uforumserver/model/Post.java
+++ server/src/main/java/com/reasatrafio/uforumserver/model/Post.java
@@ -1,4 +1,29 @@
 package com.reasatrafio.uforumserver.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
 public class Post {
+    @Id
+    private String id;
+    @DBRef
+    User postedById;
+    private String name;
+    @Indexed(unique = true)
+    private String url;
+    private String description;
+    private Boolean removed;
+    private Number upvote;
+    private Number downVote;
 }
--- server/src/main/java/com/reasatrafio/uforumserver/model/User.java
+++ server/src/main/java/com/reasatrafio/uforumserver/model/User.java
@@ -6,9 +6,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
+import java.util.List;
 
 @Setter
 @Getter
@@ -25,4 +27,6 @@ public class User {
     private String password;
     private Date createdAt;
     private boolean verified;
+    @DBRef
+    List<Post> posts;
 }
